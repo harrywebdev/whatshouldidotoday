1d583dd25e178664106c55f94fa438b66534ca21
Harry B <harry@gethornet.com>
2022-08-30 13:21:43 +0200

    Install tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,9 @@
 import { Link, LiveReload, Outlet } from "@remix-run/react"
+import styles from "./styles/app.css"
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }]
+}
 
 export default function App() {
   return (

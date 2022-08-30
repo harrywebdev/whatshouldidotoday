70dc1bbcafcf81e5e3e961212445e8b9f534d420
Harry B <harry@gethornet.com>
2022-08-30 12:48:27 +0200

    Basic routing

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,46 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+import { Link, LiveReload, Outlet } from "@remix-run/react"
 
 export default function App() {
   return (
     <html lang="en">
       <head>
-        <Meta />
-        <Links />
+        <meta charSet="utf-8" />
+        <title>What should I do today?</title>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        <header>
+          <h1>What should I do today?</h1>
+          <nav>
+            <menu>
+              <li>
+                <Link to="/" title="Today" aria-label="Today">
+                  [ Today ]
+                </Link>
+              </li>
+              <li>
+                <Link to="/history" title="History" aria-label="History">
+                  [ History ]
+                </Link>
+              </li>
+              <li>
+                <Link to="/todos" title="TODOs" aria-label="TODOs">
+                  [ TODOs ]
+                </Link>
+              </li>
+            </menu>
+          </nav>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+        <footer>
+          <p>
+            &copy; What should I do today? by{" "}
+            <a href="https://github.com/harrywebdev">@harrywebdev</a>
+          </p>
+        </footer>
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
---
+++ app/routes/todos.tsx
@@ -0,0 +1,3 @@
+export default function TodosRoute() {
+  return <div>TODOs Route</div>
+}

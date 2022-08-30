d36cced7a6bd893d558d41a1615d85770c997251
Harry B <harry@gethornet.com>
2022-08-30 18:10:14 +0200

    Add marking log todo as done

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,8 @@
-import type { LoaderFunction } from "@remix-run/node"
+import type { ActionFunction, LoaderFunction } from "@remix-run/node"
 import { db } from "~/utils/db.server"
 import type { Prisma } from "@prisma/client"
-import { json } from "@remix-run/node"
-import { Link, useLoaderData } from "@remix-run/react"
+import { json, redirect } from "@remix-run/node"
+import { Link, useActionData, useLoaderData } from "@remix-run/react"
 import { format, parseISO } from "date-fns"
 
 type DailyLogWithTodos = Prisma.DailyLogGetPayload<{
@@ -69,8 +69,44 @@ export const loader: LoaderFunction = async () => {
   return json({ dailyLog })
 }
 
+type ActionData = {
+  formError?: string
+}
+
+const badRequest = (data: ActionData) => json(data, { status: 400 })
+
+export const action: ActionFunction = async ({ request }) => {
+  const form: FormData = await request.formData()
+
+  const id = form.get("id")
+
+  if (typeof id !== "string") {
+    return badRequest({
+      formError: `Invalid item ID`,
+    })
+  }
+
+  console.log("id", id)
+
+  // make sure we find it
+  await db.logTodo.findUniqueOrThrow({ where: { id } })
+
+  // mark as done
+  await db.logTodo.update({
+    data: {
+      isDone: true,
+    },
+    where: {
+      id,
+    },
+  })
+
+  return redirect("/")
+}
+
 export default function IndexRoute() {
   const { dailyLog } = useLoaderData<LoaderData>()
+  const actionData = useActionData<ActionData>()
 
   return (
     <>
@@ -78,13 +114,22 @@ export default function IndexRoute() {
         <h2>Today: {format(parseISO(dailyLog.logDate), "do MMMM, y")}</h2>
       </header>
       <section>
+        {actionData?.formError && (
+          <p className="text-red-600">{actionData?.formError}</p>
+        )}
         <ul>
           {dailyLog.logTodos.map((todo) => {
             return (
               <li key={todo.id}>
-                <strong>{todo.title}</strong>
+                <strong>
+                  {todo.isDone ? "☑" : "☐"} {todo.title}
+                </strong>
                 <br />
                 {todo.description}
+                <form method="post" action="?index">
+                  <input type="hidden" name="id" value={todo.id} />
+                  <button type="submit">Mark as Done</button>
+                </form>
               </li>
             )
           })}

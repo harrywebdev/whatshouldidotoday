808690151c9ca41fb278d6ea0a56ace8b4cc1aa5
Harry B <harry@gethornet.com>
2022-08-30 15:04:46 +0200

    Load TODOs

--- app/routes/todos/index.tsx
+++ app/routes/todos/index.tsx
@@ -1,13 +1,57 @@
-import { Link } from "@remix-run/react"
+import { Link, useLoaderData } from "@remix-run/react"
+import type { LoaderFunction } from "@remix-run/node"
+import type { Todo } from "@prisma/client"
+import { db } from "~/utils/db.server"
+import { json } from "@remix-run/node"
+
+type LoaderData = { todos: Todo[] }
+
+export const loader: LoaderFunction = async () => {
+  // TODO: filter by `repeat` according to current day of week
+
+  const data: LoaderData = {
+    todos: await db.todo.findMany({
+      orderBy: [
+        {
+          sequence: "asc",
+        },
+      ],
+    }),
+  }
+
+  return json(data)
+}
 
 export default function TodosIndexRoute() {
+  const data = useLoaderData<LoaderData>()
+
   return (
-    <header>
-      <nav>
-        <Link to="/todos/new" title="Add New" aria-label="Add New">
-          Add New
-        </Link>
-      </nav>
-    </header>
+    <>
+      <header>
+        <nav>
+          <Link to="/todos/new" title="Add New" aria-label="Add New">
+            Add New
+          </Link>
+        </nav>
+      </header>
+      <section>
+        <h2>Current TODOs</h2>
+        <ul>
+          {data.todos.map((todo) => {
+            return (
+              <li key={todo.id}>
+                <strong>{todo.title}</strong>
+                {todo.description && (
+                  <>
+                    <br />
+                    {todo.description}
+                  </>
+                )}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </>
   )
 }
